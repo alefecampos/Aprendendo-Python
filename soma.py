ffb7fea9004a01341d03cac13f8063aac182d9b3
alefecampos <camposalefe@gmail.com>
2026-03-26 12:15:46 -0300

    Update and rename ex003(soma).py to soma.py

--- soma.py
+++ soma.py
@@ -1,5 +1,6 @@
+#input, soma e resultados com formatação de texto
 n1 = int( input('Digite um número: '))
 n2 = int (input('digite mais um número: '))
 s = n1 + n2
 # print('a soma entre', n1 , 'e' ,n2 , 'vale' , s) primeira tentativa. Funcionou, mas não é a melhor opção
-print('a soma entre \033[1;32m{}\033[m e \033[1;32m{}\033[m vale \033[1;30;42m{}\033[m '.format(n1, n2, s))
+print(f'a soma entre \033[1;32m{n1}\033[m e \033[1;32m{n2}\033[m vale \033[1;30;42m{s}\033[m .')
