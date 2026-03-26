16bd80bc2e5ff59ba98e98590e756c1c23b0584d
alefecampos <camposalefe@gmail.com>
2026-03-26 12:13:24 -0300

    Update and rename ex002(Prazer em te conhecer).py to Prazer em te conhecer.py

---
+++ Prazer em te conhecer.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome? ')
+
+print(f'Olá \033[1;32m{nome}\033[m, é um prazer te conhecer!')
+
+#aprendendo input e formatação de texto
--- ex002(Prazer em te conhecer).py
+++
@@ -1,3 +0,0 @@
-nome = input('Qual é o seu nome? ')
-
-print('Olá \033[1;32m{}\033[m, é um prazer te conhecer!'.format(nome))
