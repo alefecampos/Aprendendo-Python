f8a6dd0b97a9552e9ba47022177e23f12a161089
alefecampos <camposalefe@gmail.com>
2026-04-09 09:53:49 -0300

    Update and rename ex009(tabuada).py to 09 - tabuada 1.0.py

---
+++ 09 - tabuada 1.0.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número para saber sua tabuada: '))
+print('-'*12)
+print(f'A tabuada de \033[1;31m{n}\033[m é: \n \033[1;4;33m1x{n:2}= {n:2} \n 2x{n:2}= {n*2:2} \n 3x{n:2}= {n*3:2} \n 4x{n:2}= {n*4:2} \n 5x{n:2}= {n*5:2} \n 6x{n:2}= {n*6:2} \n 7x{n:2}= {n*7:2} \n 8x{n:2}= {n*8:2} \n 9x{n:2}= {n*9:2} \n 10x{n:1}= {n*10:1}\033[m')
+print('-'*12)
