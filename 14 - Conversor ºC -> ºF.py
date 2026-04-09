172ab6f2501e9b394d3facc516ebf469d9de0ef5
alefecampos <camposalefe@gmail.com>
2026-04-09 10:46:03 -0300

    Update and rename ex014(ºC - ºF).py to 14 - Conversor ºC -> ºF.py

---
+++ 14 - Conversor ºC -> ºF.py
@@ -0,0 +1,4 @@
+c = float(input('Qual a temperatura em ºC? '))
+print(f'A temperatura de {c}ºC equivale a {32+(c*1.8)}ºF')
+
+# outra forma de calcular é f = 9*c/5 (para calcular Fahrenheit em geral);
