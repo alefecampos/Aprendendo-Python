86cd62bd9fa5d72b03addc64b5714c1c6670a275
alefecampos <camposalefe@gmail.com>
2026-04-09 09:30:16 -0300

    Update ex005(Antecessor e sucessor).py

--- ex005(Antecessor e sucessor).py
+++ ex005(Antecessor e sucessor).py
@@ -1,4 +1,2 @@
 n = int(input('Digite um número: '))
-print('Você digitou o número \033[0;31m{}\033[m. Seu antecessor é \033[0;34m{}\033[m, e seu sucessor é \033[0;33m{}\033[m' .format(n, n-1, n+1 ))
-
-#problemas: o n não precisa de parênteses; Foi necessário adicionar a variável int antes do input;
+print(f'Você digitou o número \033[0;31m{n}\033[m. Seu antecessor é \033[0;34m{n-1}\033[m, e seu sucessor é \033[0;33m{n+1}\033[m')
