54c7506b92bb4c48e057113de9a84788a8ca4d82
alefecampos <camposalefe@gmail.com>
2026-04-09 10:51:11 -0300

    Update and rename ex015(kb - dia).py to 15 - aluguel de carro Km + diária.py

--- 15 - aluguel de carro Km + diária.py
+++ 15 - aluguel de carro Km + diária.py
@@ -2,4 +2,4 @@ p = int(input('Quantas diárias foram consumidas? '))
 d = float(input('Quantos kilômetros foram percorridos? '))
 
 valor = (p*60)+(d*0.15)
-print('Foram consumidas {} diárias e percorrida a distância de {} km. Logo, o valor a ser pago é de R$ {:.2f}' .format(p, d, valor))
+print(f'Foram consumidas {p} diárias e percorrida a distância de {d} km. Logo, o valor a ser pago é de R$ {valor:.2f}')
