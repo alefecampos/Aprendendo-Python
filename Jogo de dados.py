8278aed1618ee074a460c76232c89dbb5219d196
alefecampos <camposalefe@gmail.com>
2026-04-09 09:29:08 -0300

    Update Jogo de dados.py

--- Jogo de dados.py
+++ Jogo de dados.py
@@ -1,5 +1,7 @@
 from random import randint
 from time import sleep
+
+
 dado1 = randint(1,6)
 dado2 = randint(1,6)
 print('-='*8)
@@ -7,4 +9,4 @@ print('Jogando dados...')
 print('-='*8)
 sleep(1)
 sleep(1)
-print('Você obteve \033[1;4;30;47m{}\033[m e \033[1;4;30;47m{}\033[m' .format(dado1,dado2))
+print(f'Você obteve \033[1;4;30;47m{dado1}\033[m e \033[1;4;30;47m{dado2}\033[m')
