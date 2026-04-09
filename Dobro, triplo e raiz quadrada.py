e6fd6e26a9f2b32265c8ee1333ae6db347173e22
alefecampos <camposalefe@gmail.com>
2026-04-09 09:35:44 -0300

    Update and rename ex006(x2, x3 e RQ).py to Dobro, triplo e raiz quadrada.py

---
+++ Dobro, triplo e raiz quadrada.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número para saber seu dobro, triplo e raiz quadrada: '))
+print(f'o dobro de \033[1;31m{n}\033[m é \033[1;32m{n*2}\033[m, o triplo é \033[1;32m{n*3}\033[m, e a raiz quadrada é \033[1;32m{n**(1/2)}\033[m')
--- ex006(x2, x3 e RQ).py
+++
@@ -1,2 +0,0 @@
-n = int(input('Digite um número para saber seu dobro, triplo e raiz quadrada: '))
-print('o dobro de \033[1;31m{}\033[m é \033[1;32m{}\033[m, o triplo é \033[1;32m{}\033[m, e a raiz quadrada é \033[1;32m{}\033[m' .format(n, n*2, n*3, n**(1/2)))
