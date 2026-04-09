67702bd56d5174fd2e62126def6a2ffbb422bd87
alefecampos <camposalefe@gmail.com>
2026-04-09 09:56:09 -0300

    Update and rename ex011(pintura).py to 11 - pintura.py

---
+++ 11 - pintura.py
@@ -0,0 +1,4 @@
+n1 = float(input('Qual a altura da parede? '))
+n2 = float(input('Qual a largura da parede? '))
+
+print(f'Você vai precisar de \033[1;34;41m{n1*n2/2}\033[m litros de tinta para pintar a área de \033[1;34;41m{n1*n2}m²\033[m')
