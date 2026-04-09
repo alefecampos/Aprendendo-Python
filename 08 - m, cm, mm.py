e3b7d8ff7da4e08682bac5869f76570edaf6b1b4
alefecampos <camposalefe@gmail.com>
2026-04-09 09:43:55 -0300

    Update and rename ex008(m, cm, mm).py to 08 - m, cm, mm.py

---
+++ 08 - m, cm, mm.py
@@ -0,0 +1,2 @@
+m = float(input('digite o valor em metros: '))
+print(f'você tem \033[1;30;47m{m}\033[m metros. Isso equivale a \033[1;30;47m{m*100}\033[m centímetros e \033[1;30;47m{m*1000}\033[m milímetros')
--- ex008(m, cm, mm).py
+++
@@ -1,2 +0,0 @@
-m = float(input('digite o valor em metros: '))
-print('você tem \033[1;30;47m{}\033[m metros. Isso equivale a \033[1;30;47m{}\033[m centímetros e \033[1;30;47m{}\033[m milímetros' .format(m, m*100, m*1000))
